5f402a2939f219ebe10198c3efa628fa66c8332c
Mateusz Kwieciński <mateusz.kwiecinski@tooploox.com>
2019-07-19 20:33:17 +0200

    Migrate to androidx

--- androidApp/src/main/java/sample/MainActivity.kt
+++ androidApp/src/main/java/sample/MainActivity.kt
@@ -1,15 +1,20 @@
 package sample
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import events.GetEvents
+import sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val usecase = GetEvents()
         val osiem = usecase()
+        binding.mainText.text = "items count = ${osiem.size}"
     }
 }
