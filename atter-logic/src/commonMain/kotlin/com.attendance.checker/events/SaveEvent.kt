7fc3c620813b4240cffab9ea549eee7c9f8d325a
Patryk Mieszała <patryk.mieszala@gmail.com>
2019-07-22 20:46:13 +0200

    edit existing event

--- atter-logic/src/commonMain/kotlin/com.attendance.checker/events/SaveEvent.kt
+++ atter-logic/src/commonMain/kotlin/com.attendance.checker/events/SaveEvent.kt
@@ -2,7 +2,13 @@ package com.attendance.checker.events
 
 class SaveEvent {
     fun save(event: Event): Event? {
-        events.add(0, event)
+        val index = events.indexOf(event)
+
+        if (index != -1) {
+            events[index] = event
+        } else {
+            events.add(0, event)
+        }
 
         return event
     }
